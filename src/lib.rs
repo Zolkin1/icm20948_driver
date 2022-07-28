831e5083bd054d4d28e9fecb6fe2b1eceb386325
Zolkin1 <zach.olkin@gmail.com>
2022-07-28 15:57:32 -0600

    Some more documentation changes.

--- src/lib.rs
+++ src/lib.rs
@@ -5,16 +5,19 @@
 //! The driver depends on embedded-hal, so as long as the HAL you use implements those traits, then
 //! this driver should be compatible.
 //!
-//! The driver is split into two parts: an I2C part and an SPI part. All functions are implemented for both bus types.
+//! The data sheet for this device can be found [here](https://invensense.tdk.com/download-pdf/icm-20948-datasheet/).
 //!
-//! You can instantiate multiple objects if you have multiple IMUs.
+//! The driver is split into two parts: an I2C part and an SPI part. All functions are implemented for both bus types.
 //!
 //! Extensive testing has not been done on the LPFs and data rates, although that will come soon.
 //!
-//! The driver assumes control of the bus and thus it is recommended to use shared-bus in a multi-threaded context
+//! You can instantiate multiple objects if you have multiple IMUs. The driver assumes control of
+//! the bus and thus it is recommended to use shared-bus.
 //!
 //! Currently, there is no support for the magnetometer, the sensor acting like an I2C master, the FIFO, the FSYNC, or the DMP.
 //! I am hoping to add support for the FIFO and magnetometer soon. Using the sensor as an I2C master over the auxiliary bus is unlikely to ever happen.
+//!
+//! Examples can be found [here](https://github.com/Zolkin1/icm20948_driver/tree/main/examples).
 //!
 //! The github repo can be found [here](https://github.com/Zolkin1/icm20948_driver).
 
